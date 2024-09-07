b497133297c3389bb1868b8a934debced591ec08
14mohamedahmed <mohamed.ahmed@kickstartinteractive.com>
2024-09-07 23:42:02 +0300

    handle splash screen and the app icon

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Utopia Store',
+      showSemanticsDebugger: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Constants.mainColor),
         useMaterial3: true,
